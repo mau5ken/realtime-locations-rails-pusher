f9d76daa71408b794ac492b240c8b1eb0988e70f
ken <harygato3@gmail.com>
2019-09-30 02:44:30 -0500

    added coffescript for logic sites and url displaying maps

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,9 +2,20 @@ class TripsController < ApplicationController
   def index
   end
 
+  # function for creating a new trip
   def create
+    @trip = Trip.new(trip_params)
+    @trip.checkins.build(lat: params[:lat], lng: params[:lng])
+    render json: @trip.as_json if @trip.save
   end
 
+  # function for showing a trip
   def show
+    @trip = Trip.find_by(uuid: params[:id])
   end
+
+  private
+    def trip_params
+      params.permit(:name)
+    end
 end
