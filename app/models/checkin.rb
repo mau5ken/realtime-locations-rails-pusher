a00430d46ea45a05fcecbd43ba1965e74615cfd6
ken <harygato3@gmail.com>
2019-09-30 03:34:06 -0500

    Removed error form aplication.html.erb Added correctly apu pusher

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,3 +1,9 @@
 class Checkin < ApplicationRecord
   belongs_to :trip
-end
+  after_create :notify_pusher
+
+  # method to publish a user's current location
+  def notify_pusher
+    Pusher.trigger('location', 'new', self.trip.as_json)
+  end
+end 
