beb8575e24eaa50a8a284eb4e40926604fb5ecb4
ken <harygato3@gmail.com>
2019-09-30 02:01:44 -0500

    migrated db and created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'checkins/create'
-  get 'trips/index'
-  get 'trips/create'
-  get 'trips/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trips do
+    resources :checkins, only: :create
+  end
+  root 'trips#index'
 end
